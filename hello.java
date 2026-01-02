38922754ef2d759e3e0520abd64c6b632081887b
OmarMUhammed03 <omarb2515@gmail.com>
2026-01-02 14:16:23 +0200

    add new year

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        int x = 10;
-	System.out.println("Hello from Ahmed Ramadan " + x);
+        int x = 2026;
+        System.out.println("Year: " + x);
     }
 }
