3eaabe2443bc5e0bfbbf7045f1facc130b47a279
Ahmed Ramadan <=>
2026-01-02 14:18:40 +0200

    conflict again

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         int x = 2026;
         System.out.println("Year: " + x);
+        System.out.println("conflict will be here");
     }
 }
