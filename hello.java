e5dccfc65121c1f7ca7a4b4dd26219376f3f05e0
OmarMUhammed03 <omarb2515@gmail.com>
2026-01-02 14:14:49 +0200

    remove the new line

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello {
     public static void main(String[] args) {
         int x = 10;
         System.out.println("Hello, World! " + x);
-        System.out.println("dummy-feature.");
+        
     }
 }
