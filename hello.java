4689316a3114c35f21347899306690201813662b
OmarMUhammed03 <omarb2515@gmail.com>
2026-01-02 14:41:18 +0200

    Revert "add asfl l;v"
    agkla
    his reverts commit d7f43496b29e8ce95d6814c2b88eda3df81ccdd1.
    
    revert commit#

--- hello.java
+++ hello.java
@@ -2,6 +2,5 @@ public class hello {
     public static void main(String[] args) {
         int x = 2026;
         System.out.println("Year: " + x);
-        System.out.println("Hello, aa;lksfgkl;as world");
     }
 }
