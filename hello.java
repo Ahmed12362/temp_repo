a8f15b0a26637bcda68bc9d234907325e73036c2
Ahmed Abdelrahman <mizobek74@gmail.com>
2026-01-02 14:21:41 +0200

    conflict from iiiAhmed

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello {
         int x = 2026;
         System.out.println("Year: " + x);
         System.out.println("Hello, aa;lksfgkl;as world");
+        System.out.println("This is iiiAhmed");
     }
 }
